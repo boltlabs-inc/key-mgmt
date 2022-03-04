886121202b84a55d4baf2709fe1a24d12f55cf37
Gijs Van Laer <gijs.vanlaer@gmail.com>
2022-03-04 10:41:26 +0100

    Basic setup #2

---
+++ src/bin/client.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_client_runs() {
+        main()
+    }
+}
---
+++ src/bin/server.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_server_runs() {
+        main()
+    }
+}
