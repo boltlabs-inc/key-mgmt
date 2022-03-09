497ebb01cb4bba513cbf8642b368aa8d78761b8b
Gijs Van Laer <gijs.vanlaer@gmail.com>
2022-03-09 13:59:14 +0100

    Add extra warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+//! This crate is an implementation of a key management system
 
+#![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
+#![warn(unused_results)]
+#![warn(future_incompatible)]
+#![warn(unused)]
+#![forbid(rustdoc::broken_intra_doc_links)]
