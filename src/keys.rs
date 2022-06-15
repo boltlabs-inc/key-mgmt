b605d75f1bda4d5f2662edad3fe7d3842aafdc12
indomitableSwan <swan@boltlabs.io>
2022-06-15 12:35:02 -0400

    minor edits to documentation

--- src/keys.rs
+++ src/keys.rs
@@ -45,19 +45,19 @@ where
     key_info: KeyInfo,
 }
 
-/// A use permission is a type that defines an authorization policy for an
-/// digital asset key or key share. An authorization policy describes the degree
-/// to which the asset owner has delegated custody to another entity.
+/// A use permission is a type that defines the degree to which the asset owner
+/// has delegated custody of a digital asset key or key share to another entity.
 ///
 /// TODO (design): This abstraction relies on the design of [`Delegated`] and
-/// [`Passive`] key types and their capabilities, which is an open design
-/// question.
+/// [`Passive`] key types and their capabilities, which are open design
+/// questions.
 pub trait UsePermission {}
 
-/// Authorization policy that requires an asset owner to participate in any
-/// action involving the digital asset key. The asset owner can set a
-/// [`UserPolicySpecification`] to apply additional restrictions on digital
-/// asset key usage.
+/// Use permission that assigns key custody to the asset owner. This permission
+/// requires an asset owner to actively participate in any action involving the
+/// digital asset key by authenticating with their password. The asset owner can
+/// set a [`UserPolicySpecification`] to apply additional rules and requirements
+/// for digital asset key usage.
 
 #[derive(Debug)]
 #[allow(unused)]
@@ -66,12 +66,12 @@ struct SelfCustodial {
 }
 impl UsePermission for SelfCustodial {}
 
-/// Authorization policy that delegates signing authority to a specified
+/// Use permission that delegates signing authority to a specified
 /// delegated party. The asset owner can set a [`UserPolicySpecification`] to
-/// apply additional restrictions on digital asset key usage.
+/// apply additional rules and requirements for digital asset key usage.
 ///
-/// TODO (design): Defining the specific authorization policy is an open design
-/// question. See also the note on [`UsePermission`].
+/// TODO (design): Defining the specification for `Delegated` use permission
+/// is an open design question. See also the note on [`UsePermission`].
 ///
 /// TODO (design, implementation): Add a field describing the designated signing
 /// authority / delegated party. Figure out how to represent such an authority
@@ -83,13 +83,14 @@ struct Delegated {
 }
 impl UsePermission for Delegated {}
 
-/// Authorization policy that refers to a digital asset key that was created
-/// without active input from the asset owner. The entity with custody has
-/// complete signing authority and does not get any [`UserPolicySpecification`]
-/// or other custody-related input from the asset owner.
+/// Use permission that refers to a digital asset key that was created by
+/// the Service Provider on behalf of an asset owner, i.e., the asset owner does
+/// not have a registered account with the key servers. The Service Provider has
+/// custody of the key, including completel signing authority, i.e., there is
+/// no [`UserPolicySpecification`] set.
 ///
-/// TODO (design): Defining the specific authorization policy is an open design
-/// question. See also the note on [`UsePermission`].
+/// TODO (design): Defining the specification for `Passive` use permission is an
+/// open design question. See also the note on [`UsePermission`].
 ///
 /// TODO (design, implementation): Add a field describing the designated signing
 /// authority. Figure out how to represent such an authority and how many might
@@ -98,7 +99,7 @@ impl UsePermission for Delegated {}
 struct Passive;
 impl UsePermission for Passive {}
 
-/// Specification of a user policy. This describes the set of policies that an
+/// Specification of a user-set policy. This describes the set of policies that an
 /// asset owner can define around use of a digital asset key that are enforced
 /// by the DAMS key servers. If the key servers find that a user-set policy has
 /// been violated, they can require additional validation directly from the
@@ -112,7 +113,7 @@ pub struct UserPolicySpecification {}
 /// the use of a digital asset key.
 pub trait UseRestriction {}
 
-/// Use restriction that assigns veto power to a set of asset fiduciaries.
+/// Use restriction that assigns veto power to a given set of asset fiduciaries.
 ///
 /// Assumption: This module will be used with a DAMS instantiation that has a
 /// fixed set of asset fiduciaries, configured at system setup. The asset
@@ -125,9 +126,9 @@ pub trait UseRestriction {}
 struct SharedControl;
 impl UseRestriction for SharedControl {}
 
-/// Use restriction that limits veto power only to the asset owner; that is,
-/// given a valid, authenticated request to use a digital asset key, no
-/// additional parties are consulted.
+/// Use restriction that does not assign veto power to a given set of asset
+/// fiduciaries; that is, given a valid, authenticated request to use a digital
+/// asset key, no additional parties are consulted.
 #[derive(Debug)]
 struct Unilateral;
 impl UseRestriction for Unilateral {}
