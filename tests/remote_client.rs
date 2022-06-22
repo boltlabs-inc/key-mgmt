e5764814a010b3f4a6e31c92d251154cfd017fd2
Marcella Hastings <marcellahastings@gmail.com>
2022-06-22 10:50:57 -0400

    add remote client component tests #35

---
+++ tests/remote_client.rs
@@ -0,0 +1,36 @@
+use da_mgmt::{
+    keys::{KeyId, SharedControl, UserId},
+    remote_client::*,
+    transaction::TransactionApprovalRequest,
+};
+
+#[test]
+#[should_panic(expected = "not yet implemented")]
+fn create_passive_digital_asset_key_not_implemented() {
+    let _result = create_passive_digital_asset_key(UserId, SharedControl);
+}
+
+#[test]
+#[should_panic(expected = "not yet implemented")]
+fn request_transaction_signature_not_implemented() {
+    let tar = TransactionApprovalRequest::default();
+    let _result = request_transaction_signature(tar);
+}
+
+#[test]
+#[should_panic(expected = "not yet implemented")]
+fn retrieve_public_keys_not_implemented() {
+    let _result = retrieve_public_keys(UserId);
+}
+
+#[test]
+#[should_panic(expected = "not yet implemented")]
+fn retrieve_public_key_by_id_not_implemented() {
+    let _result = retrieve_public_key_by_id(UserId, &KeyId);
+}
+
+#[test]
+#[should_panic(expected = "not yet implemented")]
+fn retrieve_audit_log_not_implemented() {
+    let _result = retrieve_audit_log(UserId, None);
+}
