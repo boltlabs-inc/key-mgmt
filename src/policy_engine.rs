eaf5433bc88bc8564c08c79c45bcff8e86fe6686
Marcella Hastings <marcellahastings@gmail.com>
2022-06-29 17:37:51 -0400

    add policy engine config specification #14
    also fix some docs around policy engine

--- src/policy_engine.rs
+++ src/policy_engine.rs
@@ -3,7 +3,49 @@
 //! The key server will interact with a [`PolicyEngine`] instantiation.
 
 use crate::transaction::TransactionApprovalRequest;
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
+/// Configuration for an asset fiduciary.
+///
+/// This must include public key information that can be used to verify approvals.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AssetFiduciaryConfig;
+
+/// Configuration for a policy engine.
+///
+/// Assumption: The access control structure for asset fiduciaries requires approval
+/// from all fiduciaries.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PolicyEngineConfig {
+    /// Indicates whether unilateral control keys are allowed in the system.
+    #[serde(default)]
+    unilateral_control_allowed: bool,
+    /// The number of [`SelfCustodial`](crate::keys::SelfCustodial) keys each user can create.
+    #[serde(default)]
+    max_self_custodial: u16,
+    /// The number of [`Delegated`](crate::keys::Delegated) keys each user can create.
+    #[serde(default)]
+    max_delegated: u16,
+    /// The set of asset fiduciaries.
+    asset_fiduciaries: Vec<AssetFiduciaryConfig>,
+}
+
+#[allow(unused)]
+impl PolicyEngineConfig {
+    pub fn unilateral_control_allowed(&self) -> bool {
+        self.unilateral_control_allowed
+    }
+    pub fn max_self_custodial(&self) -> u16 {
+        self.max_self_custodial
+    }
+    pub fn max_delegated(&self) -> u16 {
+        self.max_delegated
+    }
+    pub fn asset_fiduciaries(&self) -> &Vec<AssetFiduciaryConfig> {
+        &self.asset_fiduciaries
+    }
+}
 
 /// A `PolicyEngine` is the entity responsible for coordination and management
 /// of operations requested on digital asset keys with
@@ -19,6 +61,11 @@ use thiserror::Error;
 /// will call out to the appropriate endpoints, and will handle any retries and
 /// waiting behavior dictated by the external implementation.
 pub trait PolicyEngine {
+    /// Initialize the policy engine interface according to the specified configuration.
+    fn initialize(config: PolicyEngineConfig) -> Result<Self, PolicyEngineError>
+    where
+        Self: Sized;
+
     fn request_transaction_approval(
         &self,
         request: TransactionApprovalRequest,
@@ -26,27 +73,26 @@ pub trait PolicyEngine {
 }
 
 /// The set of valid outcomes from a [`PolicyEngine`] decision.
-///
-/// TODO : Expand these variants with the appropriate context - approval
-/// signatures or a note to describe the rejection.
 #[derive(Debug)]
 #[allow(unused)]
 pub enum TransactionApprovalDecision {
+    /// All asset fiduciaries approved the request; there must be exactly one
+    /// signature per asset fiduciary.
     Approve(Vec<ApprovalSignature>),
-    Reject(RejectionContext),
+    /// At least one asset fiduciary rejected the request; the vector cannot be
+    /// empty.
+    Reject(Vec<RejectionContext>),
 }
 
 /// Signature from an asset fiduciary over a [`TransactionApprovalRequest`].
 ///
-/// Assumption: The signature is from an asset fiduciary. The set of asset
-/// fiduciaries is fixed and defined in the system configuration. This type
-/// must not be instantiated if the underlying signature does not correspond
+/// The set of asset fiduciaries is fixed and defined in the [`PolicyEngineConfig`].
+/// This type can only be instantiated if the underlying signature corresponds
 /// to one of the specified fiduciaries.
 #[derive(Debug)]
 pub struct ApprovalSignature;
 
-/// Context for why a the [`PolicyEngine`] rejected a
-/// [`TransactionApprovalRequest`].
+/// Context for why an asset fiduciary rejected a [`TransactionApprovalRequest`].
 #[derive(Debug)]
 pub struct RejectionContext;
 
