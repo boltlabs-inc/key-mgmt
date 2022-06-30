aa98a4e5a18a002d2c26ea7535f2e739a04bb9f1
Marcella Hastings <marcellahastings@gmail.com>
2022-06-30 16:03:02 -0400

    clarify key fiduciary vs service provider #35

--- src/remote_client.rs
+++ src/remote_client.rs
@@ -13,15 +13,16 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum Error {}
 
-/// Register a passive user to the key fiduciary authenticated in this session.
+/// Register a passive user to the service provider authenticated in this session.
 ///
-/// Assumption: a passive user only has [`Passive`][crate::keys::Passive] keys, which is why a key
-/// fiduciary would need to call this function to indicate that they can take
+/// Assumption: a passive user only has [`Passive`][crate::keys::Passive] keys and has
+/// not independently registered with the system, so
+/// the service provider needs to call this function to indicate that they can take
 /// actions on this user's behalf.
 ///
 /// Output: none, if successful.
 ///
-/// TODO #30 (design, implementation): Pass a session
+/// TODO #30 (design, implementation): Pass a session.
 #[allow(unused)]
 pub fn register_passive_user(user_id: UserId) -> Result<(), Error> {
     todo!()
@@ -30,11 +31,12 @@ pub fn register_passive_user(user_id: UserId) -> Result<(), Error> {
 /// Generate a new, distributed, [`Passive`][crate::keys::Passive]
 /// [`DigitalAssetKey`](crate::keys::DigitalAssetKey) with the given use
 /// restrictions for the [`UserId`], and compatible with the specified
-/// blockchain.
+/// blockchain. This must be called by the service provider over an
+/// authenticated session.
 ///
 /// Output: [`KeyInfo`] for the newly generated key, if successful.
 ///
-/// TODO #30 (design, implementation): Pass a session
+/// TODO #30 (design, implementation): Pass a session.
 #[allow(unused)]
 pub fn create_passive_digital_asset_key(
     user_id: UserId,
@@ -48,16 +50,13 @@ pub fn create_passive_digital_asset_key(
 ///
 /// Among the parameters in the [`TransactionApprovalRequest`], the [`KeyId`]
 /// must correspond to a key owned by the [`UserId`].
-/// The [`KeyId`] must correspond to a [`Passive`](crate::keys::Passive) or
-/// [`Delegated`](crate::keys::Delegated)
-/// key that have delegated signing authority to the key fiduciary authenticated
-/// in the session.
-///
-/// Assumption: A [`TransactionApprovalRequest`] originates either with the
-/// asset owner or the service provider. This is cryptographically enforced with
-/// an authenticated session between the key server and one of the asset owner
-/// or a key fiduciary. This request will fail if the calling party
-/// is not from one of those entities.
+/// The [`KeyId`] must correspond to a digital asset key in the system. If the
+/// key is a [`Passive`](crate::keys::Passive) key, the caller must be the
+/// service provider. If the key is a [`Delegated`](crate::keys::Delegated)
+/// key, the caller must be a key fiduciary who has delegated signing authority
+/// for the given key with [`KeyId`].
+/// In either case, the caller must make the request over an authenticated
+/// session.
 ///
 /// Output: If successful, returns a [`TransactionSignature`] as specified in the
 /// original [`TransactionApprovalRequest`] -- that is, over the
@@ -83,7 +82,7 @@ pub fn request_transaction_signature(
 /// Output: If successful, returns the [`KeyInfo`] for the newly imported digital asset key.
 ///
 /// TODO #25 (implementation): Pass the appropriate blockchain as a parameter.
-/// TODO #30 (design, implementation): Pass a session
+/// TODO #30 (design, implementation): Pass a session.
 #[allow(unused)]
 pub fn import_asset_key(
     user_id: UserId,
@@ -104,7 +103,7 @@ pub fn import_asset_key(
 ///
 /// Output: If successful, returns [`KeyMaterial`] corresponding to the requested key.
 ///
-/// TODO #30 (design, implementation): Pass a session
+/// TODO #30 (design, implementation): Pass a session.
 #[allow(unused)]
 pub fn export_asset_key(user_id: UserId, key_id: &KeyId) -> Result<KeyMaterial, Error> {
     todo!()
@@ -113,15 +112,18 @@ pub fn export_asset_key(user_id: UserId, key_id: &KeyId) -> Result<KeyMaterial, 
 /// Retrieve the public key info for all keys associated with the specified
 /// user from the key server.
 ///
-/// The [`KeyId`] must correspond to a [`Passive`](crate::keys::Passive) or
-/// [`Delegated`](crate::keys::Delegated) key that
-/// have delegated signing authority to the key fiduciary authenticated in the
-/// session. The [`KeyId`] must correspond to a key owned by the [`UserId`].
+/// The [`KeyId`] must correspond to a digital asset key owned by the [`UserId`].
+/// If the key is [`Passive`](crate::keys::Passive), the caller must be the
+/// service provider.
+/// If the key is [`Delegated`](crate::keys::Delegated), the caller must be a
+/// key fiduciary with delegated signing authority for the key.
+/// In either case, the request must be made by the caller over an authenticated
+/// session.
 ///
 /// Output: If successful, returns the [`KeyInfo`] for every key belonging to the
 /// user and delegated (as described) to the caller.
 ///
-/// TODO #30 (design, implementation): Pass a session
+/// TODO #30 (design, implementation): Pass a session.
 #[allow(unused)]
 pub fn retrieve_public_keys(user_id: UserId) -> Result<Vec<KeyInfo>, Error> {
     todo!()
@@ -130,14 +132,17 @@ pub fn retrieve_public_keys(user_id: UserId) -> Result<Vec<KeyInfo>, Error> {
 /// Retrieve the public key info from the key server for the specified key
 /// associated with the user.
 ///
-/// The [`KeyId`] must correspond to a [`Passive`](crate::keys::Passive) or
-/// [`Delegated`](crate::keys::Delegated) key that
-/// have delegated signing authority to the key fiduciary authenticated in the
-/// session. The [`KeyId`] must correspond to a key owned by the [`UserId`].
+/// The [`KeyId`] must correspond to a digital asset key owned by the [`UserId`].
+/// If the key is [`Passive`](crate::keys::Passive), the caller must be the
+/// service provider.
+/// If the key is [`Delegated`](crate::keys::Delegated), the caller must be a
+/// key fiduciary with delegated signing authority for the key.
+/// In either case, the request must be made by the caller over an authenticated
+/// session.
 ///
 /// Output: If successful, returns the [`KeyInfo`] for the requested key.
 ///
-/// TODO #30 (design, implementation): Pass a session
+/// TODO #30 (design, implementation): Pass a session.
 #[allow(unused)]
 pub fn retrieve_public_key_by_id(user_id: UserId, key_id: &KeyId) -> Result<KeyInfo, Error> {
     todo!()
@@ -146,11 +151,13 @@ pub fn retrieve_public_key_by_id(user_id: UserId, key_id: &KeyId) -> Result<KeyI
 /// Retrieve the audit log from the key server for a specified asset owner;
 /// optionally, filter for logs associated with the specified [`KeyId`].
 ///
-/// This will only retrieve logs for [`Passive`](crate::keys::Passive) and
-/// [`Delegated`](crate::keys::Delegated) keys that
-/// have delegated signing authority to the key fiduciary authenticated in the
-/// session. If specified, the [`KeyId`] must correspond to a key owned by the
-/// [`UserId`].
+/// The [`KeyId`] must correspond to a digital asset key owned by the [`UserId`].
+/// If the key is [`Passive`](crate::keys::Passive), the caller must be the
+/// service provider.
+/// If the key is [`Delegated`](crate::keys::Delegated), the caller must be a
+/// key fiduciary with delegated signing authority for the key.
+/// In either case, the request must be made by the caller over an authenticated
+/// session.
 ///
 /// The audit log includes context
 /// about any action requested and/or taken on the digital asset key, including
@@ -160,7 +167,7 @@ pub fn retrieve_public_key_by_id(user_id: UserId, key_id: &KeyId) -> Result<KeyI
 ///
 /// Output: if successful, returns a [`String`] representation of the logs.
 ///
-/// TODO #30 (design, implementation): Pass a session
+/// TODO #30 (design, implementation): Pass a session.
 #[allow(unused)]
 pub fn retrieve_audit_log(user_id: UserId, key_id: Option<&KeyId>) -> Result<String, Error> {
     todo!()
