1d61b2b0e6ea740b7ec752cb5c78141bee12e9e4
Marcella Hastings <marcellahastings@gmail.com>
2022-08-17 09:11:12 -0400

    change userid representation #131

--- dams/src/user.rs
+++ dams/src/user.rs
@@ -3,34 +3,27 @@
 //! Includes structs for the various models found in the first round of Mongo
 //! integration. This module will likely be split by model into sub-modules.
 
-use crate::{config::opaque::OpaqueCipherSuite, crypto::Secret, DamsError};
+use std::fmt::{Display, Write};
+
+use crate::{config::opaque::OpaqueCipherSuite, crypto::Secret};
 
 use opaque_ke::ServerRegistration;
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
 
 /// Unique ID for a user. Assumption: this will be derived from an ID generated
 /// by the Service Provider.
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
-pub struct UserId(String);
+pub struct UserId(Box<[u8; 16]>);
 
-impl ToString for UserId {
-    fn to_string(&self) -> String {
-        self.0.to_string()
-    }
-}
-
-impl FromStr for UserId {
-    type Err = DamsError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(UserId(s.to_string()))
+impl Display for UserId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("{:?}", self.0))
     }
 }
 
 impl UserId {
     pub fn as_bytes(&self) -> &[u8] {
-        self.0.as_bytes()
+        self.0.as_ref()
     }
 }
 
