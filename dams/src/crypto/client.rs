7edcefce21196f64add845690764b2c3c8fd7470
Marcella Hastings <marcellahastings@gmail.com>
2022-08-19 16:21:33 -0400

    fix documentation - remove chacha refs #107

--- dams/src/crypto/client.rs
+++ dams/src/crypto/client.rs
@@ -16,8 +16,7 @@ use super::{Encrypted, Secret};
 /// It takes the following steps:
 /// 1. Derive a master key from the [`OpaqueExportKey`]
 /// 2. Generate a new [`StorageKey`] to encrypt stored data with
-/// 3. Encrypt the storage key under the master key, using the ChaCha20Poly1305
-/// AEAD scheme
+/// 3. Encrypt the storage key under the master key, using an AEAD scheme
 /// 4. Return the encrypted storage key
 #[allow(unused)]
 pub fn create_and_encrypt_storage_key(
@@ -32,8 +31,7 @@ pub fn create_and_encrypt_storage_key(
 ///
 /// This must be run by the client. It takes the following steps:
 /// 1. Generates a new secret
-/// 2. Encrypt it under the [`StorageKey`], using the ChaCha20Poly1305 AEAD
-/// scheme
+/// 2. Encrypt it under the [`StorageKey`], using an AEAD scheme
 #[allow(unused)]
 pub fn create_and_encrypt_secret(
     rng: impl CryptoRng + RngCore,
