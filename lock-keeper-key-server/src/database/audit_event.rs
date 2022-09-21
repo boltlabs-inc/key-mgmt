d9a653140390916409636a378e5f5a03e97e0b7a
Vidya Akavoor <vidyaap@boltlabs.io>
2022-09-21 16:32:31 -0400

    Remove audit log extension function #232

--- lock-keeper-key-server/src/database/audit_event.rs
+++ lock-keeper-key-server/src/database/audit_event.rs
@@ -3,20 +3,13 @@
 //! Functions in this module are used to perform CRUD operations
 //! on the [`AuditEvent`] model in the MongoDB database.
 
-use crate::{
-    constants,
-    server::{Context, OperationResult},
-    LockKeeperServerError,
-};
-use async_trait::async_trait;
+use crate::{constants, LockKeeperServerError};
 use lock_keeper::{
     audit_event::{AuditEvent, Outcome},
-    channel::ServerChannel,
     crypto::KeyId,
     user::AccountName,
     ClientAction,
 };
-use std::{thread, time::Duration};
 
 use super::Database;
 
@@ -33,51 +26,5 @@ impl Database {
         let new_event = AuditEvent::new(actor.clone(), secret_id, action, outcome);
         let _ = collection.insert_one(new_event, None).await?;
         Ok(())
-    }
-}
-
-#[async_trait]
-pub trait AuditEventExt {
-    async fn log_audit_event(
-        self,
-        channel: &mut ServerChannel,
-        context: &Context,
-        action: ClientAction,
-    ) -> Result<(), LockKeeperServerError>;
-}
-
-#[async_trait]
-impl AuditEventExt for Result<OperationResult, LockKeeperServerError> {
-    async fn log_audit_event(
-        self,
-        channel: &mut ServerChannel,
-        context: &Context,
-        action: ClientAction,
-    ) -> Result<(), LockKeeperServerError> {
-        match self {
-            Ok(op_result) => Ok(context
-                .db
-                .create_audit_event(
-                    &context.account_name,
-                    op_result.0,
-                    action,
-                    Outcome::Successful,
-                )
-                .await?),
-            Err(e) => {
-                tracing::error!("{}", e);
-                if let Err(e) = channel.send_error(e).await {
-                    tracing::error!("{}", e);
-                }
-                // Log action
-                context
-                    .db
-                    .create_audit_event(&context.account_name, None, action, Outcome::Failed)
-                    .await?;
-                // Give the client a moment to receive the error before dropping the channel
-                thread::sleep(Duration::from_millis(100));
-                Ok(())
-            }
-        }
     }
 }
--- lock-keeper-key-server/src/server/operation.rs
+++ lock-keeper-key-server/src/server/operation.rs
@@ -1,14 +1,15 @@
 use async_trait::async_trait;
 use lock_keeper::{
+    audit_event::Outcome,
     channel::ServerChannel,
     types::{Message, MessageStream},
     ClientAction,
 };
+use std::{thread, time::Duration};
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status, Streaming};
 
 use crate::{
-    database::audit_event::AuditEventExt,
     server::{Context, OperationResult},
     LockKeeperServerError,
 };
@@ -38,13 +39,39 @@ pub(crate) trait Operation: Sized + Send + 'static {
         let context = context;
 
         let _ = tokio::spawn(async move {
-            let _ = self
-                .operation(&mut channel, &context)
-                .await
-                .log_audit_event(&mut channel, &context, action)
-                .await;
+            let result = self.operation(&mut channel, &context).await;
+            if let Err(e) = result {
+                Self::handle_error(&mut channel, e).await;
+                Self::audit_event(&mut channel, &context, action, Outcome::Failed).await;
+
+                // Give the client a moment to receive the error before dropping the channel
+                thread::sleep(Duration::from_millis(100));
+            } else {
+                Self::audit_event(&mut channel, &context, action, Outcome::Successful).await;
+            }
         });
 
         Ok(Response::new(ReceiverStream::new(rx)))
+    }
+    async fn handle_error(channel: &mut ServerChannel, e: LockKeeperServerError) {
+        tracing::error!("{}", e);
+        if let Err(e) = channel.send_error(e).await {
+            tracing::error!("{}", e);
+        }
+    }
+
+    async fn audit_event(
+        channel: &mut ServerChannel,
+        context: &Context,
+        action: ClientAction,
+        outcome: Outcome,
+    ) {
+        let audit_event = context
+            .db
+            .create_audit_event(&context.account_name, None, action, outcome)
+            .await;
+        if let Err(e) = audit_event {
+            let _ = Self::handle_error(channel, e);
+        };
     }
 }
