1fbc287f84342ec4661e0aed03023eed0f34a9f0
Vidya Akavoor <vidyaap@boltlabs.io>
2022-10-12 13:46:45 -0400

    Make wrapper around different stored secret types #258

--- lock-keeper/src/crypto/signing_key.rs
+++ lock-keeper/src/crypto/signing_key.rs
@@ -11,7 +11,7 @@ use super::{generic::AssociatedData, CryptoError, Encrypted, KeyId, StorageKey};
 ///
 /// This can be generated locally by the client or remotely by the server.
 #[allow(unused)]
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SigningKeyPair {
     context: AssociatedData,
 }
