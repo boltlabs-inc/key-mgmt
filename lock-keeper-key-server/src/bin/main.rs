26250e370a1199d9b7fba9738fe3b4984b6dd89a
Jordan Grace <jordan@boltlabs.io>
2022-10-13 17:56:12 -0400

    Add docker cleanup to teardown task #285

--- lock-keeper-key-server/src/bin/main.rs
+++ lock-keeper-key-server/src/bin/main.rs
@@ -3,7 +3,6 @@ use std::path::PathBuf;
 use clap::Parser;
 use lock_keeper::config::server::Config;
 use lock_keeper_key_server::server::start_lock_keeper_server;
-use tracing_subscriber::EnvFilter;
 
 #[derive(Debug, Parser)]
 pub struct Cli {
@@ -12,8 +11,7 @@ pub struct Cli {
 
 #[tokio::main]
 pub async fn main() {
-    let filter = EnvFilter::try_new("info,sqlx::query=warn").unwrap();
-    tracing_subscriber::fmt().with_env_filter(filter).init();
+    tracing_subscriber::fmt().init();
     let cli = Cli::parse();
 
     let config = Config::load(&cli.config).await.unwrap();
