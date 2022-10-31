52af22298b3db109249c646a9d4f5e3a2dba2e10
CI <CI>
2022-10-31 23:36:14 +0000

    Publishing c9d726a

--- docs/develop/lock_keeper_client_cli/command/sidebar-items.js
+++ docs/develop/lock_keeper_client_cli/command/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["AUTHENTICATE_FORMAT",""],["HELP_TEXT",""],["PRINT_FORMAT",""],["REGISTER_FORMAT",""],["REMOTE_SIGN_FORMAT",""],["RETRIEVE_FORMAT",""]],"enum":[["Command","Fully parsed command that’s ready for processing."]]};
+window.SIDEBAR_ITEMS = {"constant":[["AUTHENTICATE_FORMAT",""],["EXPORT_FORMAT",""],["HELP_TEXT",""],["PRINT_FORMAT",""],["REGISTER_FORMAT",""],["REMOTE_SIGN_FORMAT",""],["RETRIEVE_FORMAT",""]],"enum":[["Command","Fully parsed command that’s ready for processing."]]};
