c44e1023a1020b0437f2430ede8d74ec5a7f4d01
CI <CI>
2022-12-14 09:23:44 +0000

    Publishing 237903d

--- docs/develop/lock_keeper/types/operations/retrieve_secret/sidebar-items.js
+++ docs/develop/lock_keeper/types/operations/retrieve_secret/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["RetrieveContext","Options for the asset owner’s intended use of a secret"]],"mod":[["client",""],["server",""]]};
+window.SIDEBAR_ITEMS = {"enum":[["RetrieveContext","Options for the asset owner’s intended use of a secret"]],"mod":[["client",""],["server",""]],"struct":[["RetrievedSecret","Generic representation of a secret that is retrieved by a client."]]};
