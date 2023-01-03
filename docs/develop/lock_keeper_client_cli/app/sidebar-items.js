0cb952f70e83887f745b09e4a030f402a2b5d6bf
CI <CI>
2023-01-03 15:39:59 +0000

    Publishing e7b2caa

--- docs/develop/lock_keeper_client_cli/app/sidebar-items.js
+++ docs/develop/lock_keeper_client_cli/app/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["parse_cli_command",""],["parse_input","Reads next command from standard input."],["run","Runs the interactive client"]],"struct":[["Parse","Helper type to implement [GetCmdFunction]. This returns the parsing function for all commands and allows the [parse_cli_command] to iterate through these functions."]]};
+window.SIDEBAR_ITEMS = {"fn":[["parse_input","Reads next command from standard input."],["run","Runs the interactive client"],["run_script","Runs the interactive client"]]};
