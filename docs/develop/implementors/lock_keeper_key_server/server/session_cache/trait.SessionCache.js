5e8f165112fbcde7012fadda8b99ff8739e4a2b3
CI <CI>
2023-01-04 00:01:33 +0000

    Publishing d730ebe

--- docs/develop/implementors/lock_keeper_key_server/server/session_cache/trait.SessionCache.js
+++ docs/develop/implementors/lock_keeper_key_server/server/session_cache/trait.SessionCache.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"lk_session_mongodb":[["impl SessionCache for <a class=\"struct\" href=\"lk_session_mongodb/api/struct.MongodbSessionCache.html\" title=\"struct lk_session_mongodb::api::MongodbSessionCache\">MongodbSessionCache</a>"]]
+"lk_session_mongodb":[["impl <a class=\"trait\" href=\"lock_keeper_key_server/server/session_cache/trait.SessionCache.html\" title=\"trait lock_keeper_key_server::server::session_cache::SessionCache\">SessionCache</a> for <a class=\"struct\" href=\"lk_session_mongodb/api/struct.MongodbSessionCache.html\" title=\"struct lk_session_mongodb::api::MongodbSessionCache\">MongodbSessionCache</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
