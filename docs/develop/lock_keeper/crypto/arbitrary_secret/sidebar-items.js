9de0393186d7e6e936d94c8953cb1f7bb2394019
CI <CI>
2023-03-27 15:58:31 +0000

    Publishing 666a429

--- docs/develop/lock_keeper/crypto/arbitrary_secret/sidebar-items.js
+++ docs/develop/lock_keeper/crypto/arbitrary_secret/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["_DERIVE_Drop_FOR_Secret",""]],"struct":[["Secret","An arbitrary secret."]]};
+window.SIDEBAR_ITEMS = {"struct":[["Secret","An arbitrary secret."]]};
--- docs/develop/lock_keeper/crypto/data_blob/sidebar-items.js
+++ docs/develop/lock_keeper/crypto/data_blob/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["_DERIVE_Drop_FOR_DataBlob",""]],"struct":[["DataBlob","A blob of bytes for storing in the server."]]};
+window.SIDEBAR_ITEMS = {"struct":[["DataBlob","A blob of bytes for storing in the server."]]};
