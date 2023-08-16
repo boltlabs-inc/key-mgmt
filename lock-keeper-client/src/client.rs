3677d922ad080ad5c9a52261fbaf6909d22b1ed1
tposavec <tom@boltlabs.io>
2023-08-16 14:09:52 -0400

    Added a constructor for Password type.

--- lock-keeper-client/src/client.rs
+++ lock-keeper-client/src/client.rs
@@ -41,6 +41,11 @@ impl FromStr for Password {
 }
 
 impl Password {
+    // tptodo:
+    // pub fn new(bytes: &[u8]) -> Self {
+    //     Password(bytes.to_vec())
+    // }
+
     pub fn as_bytes(&self) -> &[u8] {
         &self.0[..]
     }
