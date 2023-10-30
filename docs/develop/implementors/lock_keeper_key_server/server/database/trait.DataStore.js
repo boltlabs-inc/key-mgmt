fb9d2f6770d9bffebd836274bcf20fa362f10e37
CI <CI>
2023-10-30 18:12:40 +0000

    Publishing 75b605f

--- docs/develop/implementors/lock_keeper_key_server/server/database/trait.DataStore.js
+++ docs/develop/implementors/lock_keeper_key_server/server/database/trait.DataStore.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"lock_keeper_postgres":[["impl <a class=\"trait\" href=\"lock_keeper_key_server/server/database/trait.DataStore.html\" title=\"trait lock_keeper_key_server::server::database::DataStore\">DataStore</a> for <a class=\"struct\" href=\"lock_keeper_postgres/api/struct.PostgresDB.html\" title=\"struct lock_keeper_postgres::api::PostgresDB\">PostgresDB</a>"]]
+"lock_keeper_postgres":[["impl DataStore for <a class=\"struct\" href=\"lock_keeper_postgres/api/struct.PostgresDB.html\" title=\"struct lock_keeper_postgres::api::PostgresDB\">PostgresDB</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
