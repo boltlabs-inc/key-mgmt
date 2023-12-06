5fb846fdd04573b34687b50f447ad690988ee49a
Jordan Grace <jordan@boltlabs.io>
2023-12-06 17:26:35 -0500

    moar ci

--- publish-to-local-cargo-registry.sh
+++ publish-to-local-cargo-registry.sh
@@ -1,4 +1,34 @@
+# -e exits script if any command returns an error
+set -e
+
+CRATE_VERSION=$(sed -rn 's/^version = "(.+)"/\1/p' Cargo.toml)
+echo Publishing version: $CRATE_VERSION
+
+# Checkout main branch in the registry
+cd key-mgmt-registry
+git switch main
+cd ..
+
+# Build Lock Keeper and copy to local registry
+cd lock-keeper
 cargo index add \
-    --index ./key-mgmt-registry \
+    --index ../key-mgmt-registry \
     --index-url https://fake.com \
-    --upload ./key-mgmt-registry/files/lock-keeper-client
+    --upload ../key-mgmt-registry/files/lock-keeper \
+    -- \
+    --allow-dirty
+
+# Build Lock Keeper Client and copy to local registry
+cd ../lock-keeper-client
+cargo index add \
+    --index ../key-mgmt-registry \
+    --index-url https://fake.com \
+    --upload ../key-mgmt-registry/files/lock-keeper-client \
+    -- \
+    --allow-dirty
+
+# Push local registry
+cd ../key-mgmt-registry
+git add .
+git commit -m "$CRATE_VERSION"
+git push
