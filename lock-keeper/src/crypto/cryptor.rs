1f59ead74d3467e230671a4f18bf1d05663e4e3b
Jordan <jordan.t.grace@gmail.com>
2024-03-04 14:09:10 -0500

    v2.5 dev -> QA (#636)
    Merge v2.5 to QA
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Kelly <kelly.shiptoski@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- lock-keeper/src/crypto/cryptor.rs
+++ lock-keeper/src/crypto/cryptor.rs
@@ -161,7 +161,7 @@ impl TryFrom<Vec<u8>> for Encryptor {
 
         Ok(Self {
             data,
-            context: context.try_into()?,
+            context: context.into(),
             config: config.try_into()?,
         })
     }
@@ -546,8 +546,8 @@ impl TryFrom<Vec<u8>> for Decryptor {
 
         Ok(Self {
             ciphertext,
-            context: context.try_into()?,
-            nonce: nonce.try_into()?,
+            context: context.into(),
+            nonce: nonce.into(),
             config: config.try_into()?,
         })
     }
